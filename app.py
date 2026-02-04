755756c4dd50bef5c2143489ddded5ece65b0692
Your Name <your.email@example.com>
2026-02-04 13:49:14 +0530

    adding new changes

--- app.py
+++ app.py
@@ -217,4 +217,4 @@ def server_error(error):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
